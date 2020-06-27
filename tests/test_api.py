e30d636d8a6086799a869b5509eb74587ca9f4cd
tadeo <t.corradi@gmail.com>
2020-06-27 23:04:44 +0100

    fix: test without using pandas

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,9 +2,10 @@ import io
 
 import pandas as pd
 import numpy as np
+import requests
 from fastapi.testclient import TestClient
 
-from main import make_app
+from src.main import make_app
 
 
 def make_awkward_data(nrows=100, ncols=5):
@@ -22,7 +23,7 @@ def make_awkward_data(nrows=100, ncols=5):
     return df
 
 
-def test_upload_and_download_returns_original_data():
+def test_upload_and_download_returns_original_data_with_text_and_nans():
     df = make_awkward_data(nrows=5)
     app = make_app()
     client = TestClient(app)
@@ -41,22 +42,26 @@ def test_upload_and_download_returns_original_data():
     expected = df.reset_index()
     pd.testing.assert_frame_equal(reloaded_df, expected)
 
-    # test slice
-    min_index = 1
-    max_index = 3
-    r = client.get(
-        f"datasets/csv?name={filename}&columns=index&columns=text&index_col=index&min_index={min_index}&max_index={max_index}"
-    )
 
-    assert r.status_code == 200, f"Expected 200. Got {r.json()}"
-    reloaded_df = pd.read_csv(io.StringIO(str(r.content, "utf-8")))
-    assert reloaded_df.shape == (3, 2)
-    assert reloaded_df["index"].tolist() == [1, 2, 3]
-    assert reloaded_df["text"].tolist() == ["b", "b", "c"]
 
-    # check it is listed
-    assert filename in client.get("datasets").json()
+def test_e2e():
+    app = make_app()
+    client = TestClient(app)
+    url = ""
 
-    # test deletion
-    client.delete(f"datasets?name={filename}")
-    assert filename not in client.get("dataset").json()
+    # upload csv
+    r = client.post(url + "datasets/csv",
+                      files={"file": ("e2e.csv", io.BytesIO(b't,number,text\r\n0,1,a\r\n1,2,b\r\n2,,c\r\n3,4,d\r\n'))})
+    assert r.status_code == 200, r.json()
+
+    # list all
+    r = client.get(url + "datasets")
+    assert "e2e.csv" in r.json(), r.json()
+
+    # get slice
+    r = client.get(url + "datasets/csv?name=e2e.csv&columns=t&columns=number&index_col=t&min_index=2&max_index=4")
+    assert r.content == b't,number\r\n2,\r\n3,4.0\r\n'  # stores ints as floats
+
+    # delete
+    client.delete(url + "datasets?name=e2e.csv")
+    assert "e2e.csv" not in client.get(url + "datasets")
